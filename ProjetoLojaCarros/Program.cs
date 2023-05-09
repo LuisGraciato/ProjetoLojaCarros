55d08550fe0e6cafdc5b2515559727a72ddec508
LuisGraciato <luisgraciato@hotmail.com>
2023-05-09 09:41:30 -0300

    teste

--- ProjetoLojaCarros/Program.cs
+++ ProjetoLojaCarros/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipelinesw.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
